3e0a7408f9dd95f5b60c98917ef2993d1e474352
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 03:29:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,12 +1,17 @@
-def using_include(array, element)
+'''ruby
 
+def using_include(array, element)
+  small_dogs = ["corgi","bulldog","frenchie"]
+small_dogs.include?("frenchie")
 end
 
 def using_sort(array)
-
+  small_dogs = ["corgi","bulldog","frenchie"]
+small_dogs.sort
 end
 
 def using_reverse(array)
+  small_dogs = ["corgi","bulldog","frenchie"]
 
 end
 
